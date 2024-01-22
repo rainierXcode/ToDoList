88e40e73d1a9fb8e3f92c85aa3b4dfba5262b9ce
Rainier Sapin <rainiersapin0131@gmail.com>
2024-01-22 20:59:15 +0800

    add icon

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/javascript/index/index.js',
@@ -17,9 +19,22 @@ module.exports = {
         loader: 'file-loader',
         options: {
           name: '[name].[ext]',
-          outputPath: 'assets/audio/', 
+          outputPath: 'assets/audio/',
         },
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'src/icon', 
+          to: 'assets/icons', 
+        },
+      ],
+    }),
+  ],
 };
