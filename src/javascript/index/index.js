bf11d6c1ae94a2b45beec6868d922eda950fb768
Rainier Sapin <rainiersapin0131@gmail.com>
2024-01-23 16:36:58 +0800

    Fix inresponsive

--- src/javascript/index/index.js
+++ src/javascript/index/index.js
@@ -90,3 +90,4 @@ allAddProjectEvent()
 plusButtonAddTask(document.querySelector('.add-task-container'))
 projectsShowButtonEvent()
 navBtn()
+
--- src/javascript/interaction/nav.js
+++ src/javascript/interaction/nav.js
@@ -1,11 +1,13 @@
 export function navBtn(){
    const btn = document.querySelector(".navBtn")
    const aside = document.querySelector('aside')
-   const mainContainer = document.querySelector(".main-container")
+   const article = document.querySelector('article')
+   const titleContainer = document.querySelector(".title-container")
    const headerContainer = document.querySelector(".header-container")
    btn.addEventListener('click', () =>{
     aside.classList.toggle('unShow')
-    mainContainer.classList.toggle('unShow')
+    article.classList.toggle('unShow')
+    titleContainer.classList.toggle('unShow')
     headerContainer.classList.toggle('unShow')
    })
 }
