db4170ffa78a18b72ca2c0ba8489709fddd5ce46
Rainier Sapin <rainiersapin0131@gmail.com>
2024-01-23 16:47:22 +0800

    fix header container position

--- src/javascript/interaction/nav.js
+++ src/javascript/interaction/nav.js
@@ -2,12 +2,8 @@ export function navBtn(){
    const btn = document.querySelector(".navBtn")
    const aside = document.querySelector('aside')
    const article = document.querySelector('article')
-   const titleContainer = document.querySelector(".title-container")
-   const headerContainer = document.querySelector(".header-container")
    btn.addEventListener('click', () =>{
     aside.classList.toggle('unShow')
     article.classList.toggle('unShow')
-    titleContainer.classList.toggle('unShow')
-    headerContainer.classList.toggle('unShow')
    })
 }
